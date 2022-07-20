90e53988eb2705a9bcada724a374b830e3727c7d
Young Minds <109673364+ygminds73@users.noreply.github.com>
2022-07-20 23:38:59 +0530

    Create Script4.sh

---
+++ Script4.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#This is creation and calling of shell variable --- defining variables
+Class=Young-minds
+Batch=13
+PROFESSION=AWS/DevOps
+echo "Class Name is $Class, Batch number $Batch, We are learning $PROFESSION"
