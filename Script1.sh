9fa7c2994e6a7bb2fe8638f54024c8e6d6f80434
sohansatpute <satputesohan8@gmail.com>
2022-10-20 09:44:30 +0530

    fake commit-ticketID

--- Script1.sh
+++ Script1.sh
@@ -2,3 +2,4 @@
 #This is my 1st shell script to print output
 echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello, How are you?"
+echo "Welcome to Young Minds"
--- Script2.sh
+++ Script2.sh
@@ -6,3 +6,4 @@ echo "Value of b"
 read b
 echo "Hello value of a is $a and value of b is $b"
 echo "This is sample change"
+echo "bye"
