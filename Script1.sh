e98663f4a4bc4677cfa3b2ab1430ec3b5e72218d
mukesh7668 <mukeshvarma740@gmail.com>
2023-01-04 18:06:42 +0530

    deleted 3 lines

--- Script1.sh
+++ Script1.sh
@@ -4,6 +4,4 @@ echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello, How are you?"
 echo "Welcome to Young Minds"
 echo "Best class of AWS/DevOps"
-echo "We are learning git"
-echo "We are done with git"
-echo "We have started our devops-git/github topic"
+
