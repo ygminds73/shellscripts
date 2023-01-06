d09c85cdb0d49363bc798da6069ef6d6bd921e69
unknown <prajwalmhaske307@gmail.com>
2023-01-06 21:37:17 +0530

    again add 7 lines

--- Script2.sh
+++ Script2.sh
@@ -4,3 +4,10 @@ echo "Value of a"
 read a
 echo "Value of b"
 read b
+echo "Hello value of a is $a and value of b is $b"
+echo "This is sample change"
+echo "bye"
+echo "hi"
+echo "This is sample change"
+echo "bye"
+echo "hi"
