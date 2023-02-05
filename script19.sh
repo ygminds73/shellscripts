c97a2fb99650c1672ed4e3ce1e1ae2b20c9db388
suhas1116 <122302631+suhas1116@users.noreply.github.com>
2023-02-05 17:38:58 +0530

    Update script19.sh

--- script19.sh
+++ script19.sh
@@ -7,22 +7,24 @@ do
 done
 ls -lrt
 echo "we will install pacage now"
-sleep 3
+sleep 5
 apt update -y
 apt install awscli -y
 echo "now we will create bucket"
-sleep 3
+sleep 5
 aws s3 mb s3://aws-bucket-suhas-data
-sleep 3
+sleep 5
 aws s3 ls
-sleep 3
+sleep 5
 cd /var/log/
 aws s3 cp auth.log s3://aws-bucket-suhas-data
 aws s3 ls
-sleep 3
+sleep 5
 echo "we add a kern.log file in bucket with a create folder"
-sleep 3
+sleep 5
 aws s3 cp kern.log s3://aws-bucket-suhas-data/personal/
+sleep 5
+echo "We check data"
 aws s3 ls s3://aws-bucket-suhas-data --recursive
 cd ..
 echo "complete our automation"
