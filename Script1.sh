352dd8beaa35877efb606979eca75942184bc4cd
Hulkbuster <abc@gmail.com>
2023-03-02 17:17:46 +0530

    My first

--- Script1.sh
+++ Script1.sh
@@ -6,5 +6,4 @@ echo "Welcome to Young Minds"
 echo "Best class of AWS/DevOps"
 echo "We are learning git"
 echo "We are done with git"
-echo "We have started our devops-git/github topic"
-echo "good morning"
+
