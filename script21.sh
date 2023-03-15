2415694e056e6ea5be04714a1526b279249a44b1
suhas1116 <122302631+suhas1116@users.noreply.github.com>
2023-03-16 00:10:33 +0530

    Update script21.sh

--- script21.sh
+++ script21.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #This script for load to ec2 machine and kill process with jenkins
 echo "Now we are going to install packges"
+apt-get update -y
 apt update -y
-apt -get update -y
 sleep 4
-apt install httpd -y
+apt install vsftpd -y
 sleep 4
-systemctl status httpd
-systemctl start httpd
-top
-ps aux | grep httpd
+systemctl status vsftpd
+systemctl start vsftpdd
+ps aux | grep vsftpd
+
