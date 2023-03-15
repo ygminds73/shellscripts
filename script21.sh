67657ca8e1a69435f1dd6d742f649d4ee9ee888d
suhas <suhas.dhavane111@gmail.com>
2023-03-16 00:00:48 +0530

    this for ec2 package

--- script21.sh
+++ script21.sh
@@ -1,1 +1,11 @@
 #!/bin/sh
+#This script for load to ec2 machine and kill process with jenkins
+echo "Now we are going to install packges"
+apt update -y
+sleep 4
+apt install httpd -y
+sleep 4
+systemctl status httpd
+systemctl start httpd
+top
+ps aux | grep httpd
