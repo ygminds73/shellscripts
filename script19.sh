044fb579dd2c24775f45040e2b355de89df62a9d
suhas <suhas.dhavane111@gmail.com>
2023-03-20 23:28:49 +0530

    new script

--- script19.sh
+++ script19.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-a=0
-while [ $a -lt 10 ]
-do
- echo $a
- a=`expr $a + 1`
-done
 ls -lrt
 echo "we will install pacage now"
 sleep 5
@@ -16,15 +10,16 @@ aws s3 mb s3://aws-bucket-suhas-data
 sleep 5
 aws s3 ls
 sleep 5
-cd /var/log/
-aws s3 cp auth.log s3://aws-bucket-suhas-data
+cd /home/ubuntu/Node-1/workspace/pipeline-3/
+aws s3 cp shellscripts s3://aws-bucket-suhas-data
 aws s3 ls
 sleep 5
 echo "we add a kern.log file in bucket with a create folder"
 sleep 5
-aws s3 cp kern.log s3://aws-bucket-suhas-data/personal/
+cd /home/ubuntu/Node-1/workspace/
+aws s3 cp pipeline-3 s3://aws-bucket-suhas-data/personal/
 sleep 5
-echo "We check data"
+echo "We check data now"
 aws s3 ls s3://aws-bucket-suhas-data --recursive
 cd ..
 echo "complete our automation"
