247a1cf7cdb7c5d754ade9a0953e53dc7576c384
sohansatpute <satputesohan8@gmail.com>
2023-05-01 07:38:32 +0530

    This is my first commit

--- Script1.sh
+++ Script1.sh
@@ -8,7 +8,4 @@ echo "We are learning git"
 echo "We are done with git"
 echo "This example is of branching strategy"
 echo "Hello Batch-16, We are learning Git/Github"
-echo "Good Evening"
-echo "We are changing script1"
-echo "We have applied this changes in main branch"
 
