f1ebc6d32ee0f1e2357b38a0eda3a3b380d09a75
unknown <sourabhawate45@gmail.com>
2023-05-01 22:33:35 +0530

    1st commit

--- Script1.sh
+++ Script1.sh
@@ -9,6 +9,4 @@ echo "We are done with git"
 echo "This example is of branching strategy"
 echo "Hello Batch-16, We are learning Git/Github"
 echo "Good Evening"
-echo "We are changing script1"
-echo "We have applied this changes in main branch"
 
