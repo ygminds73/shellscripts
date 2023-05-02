a983cc498d23da1a1f2ce0f30cb0c1a386754eaf
unknown <bankar15171@gmail.com>
2023-05-02 18:54:52 +0530

    this is my 6th commit

--- Script1.sh
+++ Script1.sh
@@ -5,10 +5,5 @@ echo "Hello, How are you?"
 echo "Welcome to Young Minds"
 echo "Best class of AWS/DevOps"
 echo "We are learning git"
-echo "We are done with git"
-echo "This example is of branching strategy"
-echo "Hello Batch-16, We are learning Git/Github"
-echo "Good Evening"
-echo "We are changing script1"
-echo "We have applied this changes in main branch"
+echo "We are done with
 
--- Script2.sh
+++ Script2.sh
@@ -4,7 +4,3 @@ echo "Value of a"
 read a
 echo "Value of b"
 read b
-echo "Hello value of a is $a and value of b is $b"
-echo "This is sample change"
-echo "bye"
-echo "hi"
