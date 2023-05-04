624a92eaf26ff4a92fa895ae84163a8c3500ae3e
archana-star <127712051+archana-star@users.noreply.github.com>
2023-05-04 23:49:46 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -7,4 +7,4 @@ echo "Best class of AWS/DevOps"
 echo "We are learning git"
 echo "We are done with git"
 echo "This example is of branching strategy"
-echo " I am learning devops" > index.html
+echo " I am learning devops"
