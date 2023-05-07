78fdf454be6fdaaaea6a5e49aa2886f894d290f2
archana-star <127712051+archana-star@users.noreply.github.com>
2023-05-07 18:07:08 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -3,9 +3,3 @@
 echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello, How are you?"
 echo "Welcome to Young Minds"
-echo "Best class of AWS/DevOps"
-echo "We are learning git"
-echo "We are done with git"
-echo "This example is of branching strategy"
-echo " I am learning devops"
-hii
