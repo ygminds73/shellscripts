121c0972a2d8253ae837ad142426e66e67e39369
kadam-reshma <nitu.kadam@gmail.com>
2023-05-07 13:44:25 +0530

    Mango

--- Script4.sh
+++ Script4.sh
@@ -4,4 +4,5 @@ Class=Young-minds
 Batch=13
 PROFESSION=AWS/DevOps
 echo "Class Name is $Class, Batch number $Batch, We are learning $PROFESSION"
-echo "happy sunday"
+echo "happy sunday & Monday"
+echo "happy Monday"
