f0ea4983cff7754a742ebcba90276569a811c04d
11Hemant <hemantkhandave11@gmail.com>
2023-05-10 13:51:04 +0530

    i have changes 2 script

--- script1.sh
+++ script1.sh
@@ -1,1 +1,9 @@
-echo "good morning lala bhai"
+#!/bin/bash
+#This is my 1st shell script to print output
+echo "Hello All, Welcome to AWS/Devops Class"
+echo "Hello, How are you?"
+echo "Welcome to Young Minds"
+echo "Best class of AWS/DevOps"
+echo "We are learning git"
+echo "We are done with git"
+echo "we have started our devops-git/github topic
