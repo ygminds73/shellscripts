7c70c20553149e21bd7b9c07e7f8afd13df54779
8788241192 <127922270+8788241192@users.noreply.github.com>
2023-06-01 21:22:19 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -10,4 +10,5 @@ echo "This example is of branching strategy"
 echo "Hello Batch-16, We are learning Git/Github"
 echo "Good morning batch-18"
 echo "I have created a new branch"
+echo "we are learning github"
 
