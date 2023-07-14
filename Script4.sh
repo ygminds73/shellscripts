050b96e90c9b17ccf76aa03d61e5c66e97b0ba2b
TejasJagtap11 <jagtaptejas5911@gmail.com>
2023-07-14 08:23:11 +0530

    This is my local repo

--- Script4.sh
+++ Script4.sh
@@ -3,4 +3,5 @@
 Class=Young-minds
 Batch=13
 PROFESSION=AWS/DevOps
-echo "Class Name is $Class, Batch number $Batch, We are learning $PROFESSION"
+Name=Tejas_Jagtap
+echo "Class Name is $Class, Student Name is $Name, Batch number $Batch, We are learning $PROFESSION"
