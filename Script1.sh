6c32449cc7bf21644f392799ae275699ea633e4a
ritu <ritusevta@gmail.com>
2023-09-16 01:21:41 +0530

    This is my first commit for Batch-20

--- Script1.sh
+++ Script1.sh
@@ -2,6 +2,4 @@
 #This is my 1st shell script to print output
 echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello, How are you?"
-echo "Welcome to Young Minds"
-echo "Hello batch-19, We are learning Develops"
-echo "Hello Batch-20"
+echo "Batch-20 HELLO"
