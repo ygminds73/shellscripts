6f51f11ca6b203b0a7ef05f808bd127a7aebc141
Hatim <shikalgarhatim10@gmail.com>
2023-09-16 17:19:44 +0530

    This commit is for bug fix having id 0007860

--- Script1.sh
+++ Script1.sh
@@ -8,3 +8,6 @@ echo "Hello Batch-20"
 echo "Hi this is my first change of git"
 echo "HI this change is made in feature-1 branch"
 echo "We are learning branch stratergies"
+#Here we have fixed the bug and we are sure that system is working fine. 
+echo "HI this change is made in feature-1 branch"
+echo "We are learning branch stratergies"
