adb8ff85ba12f2ce36164898490163367097325e
ritu <ritusevta@gmail.com>
2023-09-16 22:48:03 +0530

    Fix for ticket INC0007896

--- Script1.sh
+++ Script1.sh
@@ -3,5 +3,5 @@
 echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello, How are you?"
 echo "Batch-20 HELLO"
-echo "We are learning Git for branching strategy"
-echo "We are modifying code for feature-1"
+#echo "We are learning Git for branching strategy"
+#echo "We are modifying code for feature-1"
