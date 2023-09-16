0bb20a2d75a1ad867ff3aaa49dc9aa2a6862bb94
Hatim <shikalgarhatim10@gmail.com>
2023-09-16 13:04:49 +0530

    This is for script2 at line number 9

--- Script2.sh
+++ Script2.sh
@@ -6,3 +6,4 @@ echo "Value of b"
 read b
 echo "Hello value of a is $a and value of b is $b"
 echo "We are performing this practical for batch-20"
+echo "This is hatim's INC456789 for bugfix of login functionality"
