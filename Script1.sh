382e2b822ccb67db75902cd723c2deeafa9c5b40
paragtelang86 <84259110+paragtelang86@users.noreply.github.com>
2023-09-24 17:26:29 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #This is my 1st shell script to print output
 echo "Hello All, Welcome to AWS/Devops Class"
-echo "Hello, How are you?"
+echo "Hello, How are you Parag?"
 echo "Welcome to Young Minds"
 echo "Hello batch-20, We are learning Jenkins"
