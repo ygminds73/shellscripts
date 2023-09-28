823ceeb3d4d8a89cc0db7e9588f5f7ca500fda7a
Akash-Wankhade <wankhadeakash1505@gmail.com>
2023-09-28 14:19:04 +0530

    merge conflict creation branch-abc

--- Script1.sh
+++ Script1.sh
@@ -2,6 +2,7 @@
 #This is my 1st shell script to print output
 echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello, How are you?"
+echo "--------------------------------------"
 echo "Welcome to Young Minds"
 
 echo "Now merge conflict should get created"
