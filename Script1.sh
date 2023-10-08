e8103cc2deb2b4d876cad863ca3cb3add49c6d53
DevShri <rajwandhekar15@gmail.com>
2023-10-08 21:13:10 +0530

    feature practical

--- Script1.sh
+++ Script1.sh
@@ -2,4 +2,5 @@
 #This is my 1st shell script to print output
 echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello, How are you?"
-echo "AnantW New class"git
+echo "AnantW New class"
+echo "new feature branch"
