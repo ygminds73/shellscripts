ad85edf7bb4c4bc70bf4620285121aa9e0d8dbea
Amol <amolc083@gmail.com>
2023-11-20 16:35:49 +0530

    school name added succesfully

--- Script5.sh
+++ Script5.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 #This script is to make variable read only, means we cannot set the value of NAME variable again
 NAME=Young-Minds
+SCHOOL=N.M.V.Chinawal
 readonly NAME
 NAME=DEVOPS
 echo "my name is: $NAME"
+echo "my school name is:$SCHOOL"
+echo "4 and 8 line added succesfully"
