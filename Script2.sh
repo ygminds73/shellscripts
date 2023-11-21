2fd0f0692221b49bde9641a0c94b234d2d383f51
Trupti <truptidhobale4@gmail.com>
2023-11-21 13:44:02 +0530

    INC11220033 created a subnet for us-bank-loan-private and added in vpc

--- Script2.sh
+++ Script2.sh
@@ -6,4 +6,5 @@ echo "Value of b"
 read b
 echo "Hello value of a is $a and value of b is $b"
 echo "We are performing this practical for batch-20"
-echo "hello team""
+echo "hello team"
+echo"hello team we are learning branchinh strategy today"
