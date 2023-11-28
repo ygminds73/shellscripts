b0872ccfa88e69f9da5e06d066a566325e342d05
Amulya-naidu <148705418+Amulya-naidu@users.noreply.github.com>
2023-11-28 14:27:42 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -3,5 +3,5 @@
 echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello, How are you?"
 echo "Welcome to Young Minds"
-echo "Hello batch-21, We are learning Jenkins"
+echo "Hello batch-21, We are learning Jenkins all the best"
 
