adcdc5def274d629659d136be2b0104dbc0ec2cd
Amulya-naidu <naiduamulya1999@gmail.com>
2023-11-28 17:24:30 +0530

    we are learning git merge

--- Script1.sh
+++ Script1.sh
@@ -4,5 +4,5 @@ echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello, How are you?"
 echo "Welcome to Young Minds"
 echo "Hello batch-21, We are learning Jenkins all the best"
-
+echo "hiiii team"
 
