ac03604463d29e8ba4408591fcda92b257db2943
sohansatpute <satputesohan8@gmail.com>
2024-01-24 08:22:33 +0530

    Request commit

--- Script1.sh
+++ Script1.sh
@@ -5,4 +5,5 @@ echo "Hello, How are you?"
 echo "Welcome to Young Minds"
 echo "Hello batch-21, We are learning Jenkins"
 echo "Good morning batch-22, We are learning GitHub"
+echo "We are learning branching strategies"
 
