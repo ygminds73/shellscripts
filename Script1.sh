051ec25ade765a50bb5414822a5dfcd4ca8eba53
Young Minds <109673364+ygminds73@users.noreply.github.com>
2024-01-26 08:06:43 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -2,8 +2,4 @@
 #This is my 1st shell script to print output
 echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello, How are you?"
-echo "Welcome to Young Minds"
-echo "Hello batch-21, We are learning Jenkins"
-echo "Good morning batch-22, We are learning GitHub"
-echo "We are learning branching strategies"
-
+echo "We are learning Git"
