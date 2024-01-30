00b397664f049a0dd0992d3a7e1a973a36fb11cd
shajay <shajay3007@gmail.com>
2024-01-30 11:25:37 +0530

    modfiied Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #This is my 1st shell script to print output
 echo "Hello All, Welcome to AWS/Devops Class"
-echo "Hello, How are you?"
-echo "Welcome to Young Minds"
 echo "Hello batch-19, We are learning Develops"
 echo "Hello Batch-20"
