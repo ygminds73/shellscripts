20834409ace87191864c43801752262d4f471846
shajay3007 <shajay3007@gmail.com>
2024-01-30 15:23:40 +0000

    this change in scriot1.sh

--- Script1.sh
+++ Script1.sh
@@ -2,4 +2,6 @@
 #This is my 1st shell script to print output
 echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello batch-19, We are learning Develops"
-echo "Hello Batch-20"
+
+
+echo "gdg rtuyf uiiy"
