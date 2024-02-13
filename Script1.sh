870b361d9b52358e9968b2b534e02ff1e1fca0e1
shreyas <shreyasakade9@gmail.com>
2024-02-13 15:33:20 +0530

    i have changed 1 file in script 1

--- Script1.sh
+++ Script1.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-#This is my 1st shell script to print output
-echo "Hello All, Welcome to AWS/Devops Class"
-echo "Hello, How are you?"
-echo "We are learning Git"
-echo "We are trying this practical from linux EC2"
+hello world 
+hello good morning 
