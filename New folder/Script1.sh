9808dfe7df262c75b948eb77e1d371e259e1b078
Hatim <shikalgarhatim10@gmail.com>
2024-02-20 09:28:14 +0530

    This is my first ignored files commit

---
+++ New folder/Script1.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+#This is my 1st shell script to print output
+echo "Hello All, Welcome to AWS/Devops Class"
+echo "Hello, How are you?"
+echo "Welcome to Young Minds"
+echo "Hello batch-19, We are learning Develops"
+echo "Hello Batch-20"
+echo "Hi this is my first change of git"
+echo "HI this change is made in feature-1 branch"
+echo "We are learning branch stratergies"
+echo "Hi this is my first change of git"
+echo "HI this change is made in feature-1 branch"
+echo "We are doing merge and conflict will occur"
+echo "This change is made for jenkins poll SCM "
+echo "This change is made for my practice"
