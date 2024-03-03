0b450777961b1477c12a42d0b153cb2df72976c0
Sagar-ni <161938805+Sagar-ni@users.noreply.github.com>
2024-03-03 14:06:53 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #This is my 1st shell script to print output
 echo "Hello All, Welcome to AWS/Devops Class"
-echo "Hello, How are you?"
+echo "Hello, How are you buddy?"
 echo "We are learning Git"
 echo "We are learning about Git "
