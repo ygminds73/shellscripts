46bb7038ebb3cb368622594e9c7b58d40b010b54
Sagar-ni <161938805+Sagar-ni@users.noreply.github.com>
2024-03-03 16:43:36 +0530

    Update Script3.sh

--- Script3.sh
+++ Script3.sh
@@ -6,7 +6,3 @@ echo "This is my system path $PATH"
 #Set a JDK_HOME env variable
 export JDK_HOME=/bin/jdk
 echo "my new JDK home is=$JDK_HOME"
-
-TRying to check fetch command
-also finding the new errorrs
-this is devos ractice
