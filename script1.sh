933050fd509362f02559ec925b571ee160ebf8f1
AmolGore <amgtronix@gmail.com>
2024-03-30 23:35:39 +0530

    Plese merge my file

--- script1.sh
+++ script1.sh
@@ -1,5 +1,5 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-24 and learning git/gihub"
 echo "Happy weekend"
-echo " Heyy What are you doing man"
+echo " Heyy What are you doing man...... I am good baby"
 
