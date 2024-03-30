fdfc5ccdc4b6fe5f08eee5b9304588e46833916d
ReshmaAher <korade.reshma25@gmail.com>
2024-03-30 15:27:14 +0530

    delete Script-1

--- Script1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#This is my 1st shell script to print output
-echo "Hello All, Welcome to AWS/Devops Class"
-echo "Hello, How are you?"
-echo "We are learning Git"
-echo "We are trying this practical from linux EC2"
-echo "We are learning DevOps"
-echo "Today is Saturday"
-
