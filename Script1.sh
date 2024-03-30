11bb8644e83ba40c2adcdb5c201e627be2067315
sohansatpute <satputesohan8@gmail.com>
2024-03-30 09:28:28 +0530

    We have deleted 1 script file

--- Script1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#This is my 1st shell script to print output
-echo "Hello All, Welcome to AWS/Devops Class"
-echo "Hello, How are you?"
-echo "We are learning Git"
-echo "We are trying this practical from linux EC2"
-echo "We are from batch-24 and learning git/gihub"
-echo "Happy weekend"
-echo "Today is saturday"
