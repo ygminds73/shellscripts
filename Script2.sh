9b075f1bc91b58124e089d84092e2a6dcd4432c9
NilamShinde123 <nilamshinde8830gmail.com>
2024-03-30 09:39:48 +0530

    modified script 2

--- Script2.sh
+++ Script2.sh
@@ -4,7 +4,4 @@ echo "Value of a"
 read a
 echo "Value of b"
 read b
-echo "Hello value of a is $a and value of b is $b"
-echo "Welcome to DevOps"
-echo "happy good friday"
-echo 
+echo "Hello value of a is $a and value of b is $
