eb08348e29ab8e2601ced45b279be9fa9c126a02
AmolGore <amgtronix@gmail.com>
2024-04-02 08:20:09 +0530

    Hello

--- Script2.sh
+++ Script2.sh
@@ -4,5 +4,6 @@ echo "Value of a"
 read a
 echo "Value of b"
 read b
-echo "Hello value of a is $a and value of b is $b"
+echo "Hello value of a is $a and value of b is $b" 
 echo "Welcome to DevOps"
+echo hello git
