01322acae8fef670b5bfa2cbe699eb62f2af60cb
PrajwalBorse <prajwalborse101@gmail.com>
2024-04-08 15:17:53 +0530

    This is my second commit

--- script1.sh
+++ script1.sh
@@ -1,4 +1,6 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-24 and learning git/gihub"
 echo "Happy weekend"
+echo " Done with my first commit "
+echo " learning more about it"
 
