4006170fe9eb40393d9d51291b1de69a2c2e70f3
karishma42 <karishmakubde42@gmail.com>
2024-04-15 23:15:25 +0530

    added new branch related changes

--- script1.sh
+++ script1.sh
@@ -2,4 +2,5 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-24 and learning git/gihub"
 echo "Happy weekend"
 echo "hello there...."
+echo "we are learning branching stategy"
 
