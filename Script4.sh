5b5954d918ae74a28217d6e2bb1c05f2e247c1ec
Young Minds <109673364+ygminds73@users.noreply.github.com>
2024-04-18 08:17:17 +0530

    Update Script4.sh

--- Script4.sh
+++ Script4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #This is creation and calling of shell variable --- defining variables
 Class=Young-minds
-Batch=13
-PROFESSION=AWS/DevOps
+Batch=25
+PROFESSION=AWS/DevSecOps/Azure DevOps
 echo "Class Name is $Class, Batch number $Batch, We are learning $PROFESSION"
