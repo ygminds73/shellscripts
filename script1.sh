c8f5cbc18a3dec1854ab55ba81bf070bb2d98dbc
AnkitaY26 <ankitaayawale@gmail.com>
2024-05-03 14:48:23 +0530

    this is my latest commit for today

--- script1.sh
+++ script1.sh
@@ -2,5 +2,5 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-24 and learning git/gihub"
 echo "Happy weekend"
 echo "we are learning git and github from surja sir"
-#echo " we will be devops engineer soon"
-
+echo " we will be devops engineer soon"
+echo "hi i am learning devops aws cloud"
