8fca6e51a6a23b9d57561acbe8ff13d492ae8e01
akash08-ak <akashshewale0801@gmail.com>
2024-05-30 09:13:51 +0530

    This is my second git commit

--- script1.sh
+++ script1.sh
@@ -1,4 +1,7 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-24 and learning git/gihub"
-
+echo "We are trying this practical from linux EC2"
+echo "We are from batch-24 and learning git/gihub"
+echo "We are trying this practical from linux EC2"
+echo "We are from batch-24 and learning git/gihub"
 
