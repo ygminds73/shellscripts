5d9266fb341910eca994e648cd271c9ec3d424d6
SachinUmbare <sachinumbare31@gmail.com>
2024-05-30 17:57:54 +0530

    This is my first practical

--- script1.sh
+++ script1.sh
@@ -3,4 +3,6 @@ echo "We are from batch-24 and learning git/gihub"
 echo "Happy weekend"
 echo "Hello batch-24, We are learning git"
 echo "Please complete your practicals on daily basis"
+echo "hello friends"
+echo "good morning"
 
