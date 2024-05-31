391e5fb2899fc651c8392f5665e185802a4a81eb
Pratidan2925 <pratikshazende99@gmail.com>
2024-05-31 08:29:15 +0530

    modified

--- script1.sh
+++ script1.sh
@@ -3,6 +3,4 @@ echo "We are from batch-24 and learning git/gihub"
 echo "Happy weekend"
 echo "Hello batch-24, We are learning git"
 echo "Please complete your practicals on daily basis"
-
-echo "hello form Pratiksha "
-echo "bye bye "
+echo "Good Morning"
