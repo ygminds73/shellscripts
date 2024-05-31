6054d2134f2ac3b3922368f324e5cd9e470f301b
KoradeA <aarukorade2gmail.com>
2024-05-31 23:30:36 +0530

    commiteed branches

--- script1.sh
+++ script1.sh
@@ -2,5 +2,4 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-24 and learning git/gihub"
 echo "Happy weekend"
 echo "Hello batch-24, We are learning git"
-echo "Please complete your practicals on daily basis"
-
+echo "we will complete our practicals on daily basis"
