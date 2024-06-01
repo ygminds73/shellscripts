deb516c90bc6ea5e021807b3c2191f4e33e2852f
YoungMinds <ygminds73@gmail.com>
2024-06-01 09:13:21 +0530

    This is a fix for INC00023424

--- script1.sh
+++ script1.sh
@@ -1,8 +1,6 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-24 and learning git/gihub"
 echo "Happy weekend"
-echo "We are learning git"
-echo "Husband: I don't want to travel"
-echo "Wife: I want to travel"
+echo "I am fixing issue INC00023424" 
 
 
