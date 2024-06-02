05d9017a60335fd13bdb7d75110a72282ef78941
Pooja-pawar123 <pawarsangita963@gmail.com>
2024-06-02 23:30:05 +0530

    This change is for INC0902324

--- script1.sh
+++ script1.sh
@@ -2,8 +2,5 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-24 and learning git/gihub"
 echo "Happy weekend"
 echo "We are learning git"
-echo "Husband: I don't want to travel"
-echo "Wife: I want to travel"
-echo "hello batch-24,we are learning github"
 
 
