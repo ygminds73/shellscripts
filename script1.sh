7089789f2bd32fb445a80190ce7e5336e7a1268d
akash08-ak <akashshewale0801@gmail.com>
2024-06-05 15:42:15 +0530

    Branch chage 24

--- script1.sh
+++ script1.sh
@@ -1,6 +1,7 @@
-helllo
-how are you
-hi
-
-
-    
+Calories: 104
+Carbs: 28 g
+Fiber: 5 g
+Vitamin C: 10% of the Daily Value (DV)
+Copper: 6% of the DV
+Potassium: 5% of the DV
+Vitamin K: 4% of the DV
