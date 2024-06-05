b56d3b4e9bb33043ed9455e84c65fe631b848cf6
akash08-ak <akashshewale0801@gmail.com>
2024-06-05 15:21:20 +0530

    Branch chage

--- script1.sh
+++ script1.sh
@@ -1,7 +1,6 @@
-echo "We are trying this practical from linux EC2"
-echo "We are from batch-24 and learning git/gihub"
-echo "We are trying this practical from linux EC2"
-echo "We are from batch-24 and learning git/gihub"
-echo "We are trying this practical from linux EC2"
-echo "We are from batch-24 and learning git/gihub"
+helllo
+how are you
+hi
 
+
+    
