79520b298eddd3b988c34596747d7d0f4e3779b3
wankhede19 <wankhedeakash19@gmail.com>
2024-06-08 11:34:31 +0530

    this is branch sample

--- script1.sh
+++ script1.sh
@@ -4,5 +4,5 @@ echo "Happy weekend"
 echo "We are learning git"
 echo "Husband: I don't want to travel"
 echo "Wife: I want to travel"
-echo 'hello guys"
+echo 'Hello everyone , i am akash wankhede"
 
