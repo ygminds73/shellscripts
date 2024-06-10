524c61029bec537b7116fea6f856f9b53fb85df7
Amodbaghel <baghelamod012@gmail.com>
2024-06-10 14:56:51 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -4,6 +4,5 @@ echo "Happy weekend"
 echo "Hello batch-24, We are learning git"
 echo "Please complete your practicals on daily basis"
 echo "How are you?"
-echo "Hi"
 echo "Wife :i dont want to go trip"
 
