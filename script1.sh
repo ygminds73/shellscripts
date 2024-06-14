e532d651e025d02d45ae9a9050dbac1de4ade4ad
Amodbaghel <baghelamod012@gmail.com>
2024-06-14 09:06:46 +0530

    We have to slove this incident

--- script1.sh
+++ script1.sh
@@ -5,8 +5,6 @@ echo "Hello batch-24, We are learning git"
 echo "Please complete your practicals on daily basis"
 echo "How are you?"
 echo "Hi"
-echo "Husband: I dont want to travel"
-echo "Wife: I want to travel"
 
 
 
