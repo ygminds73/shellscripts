0660ba0c7d17085e4efb31d86f3ad3af63889db2
Amodbaghel <baghelamod012@gmail.com>
2024-07-01 09:24:36 +0530

    second pr

--- Script2.sh
+++ Script2.sh
@@ -4,5 +4,7 @@ echo "Value of a"
 read a
 echo "Value of b"
 read b
-echo "Hello value of a is $a and value of b is $b"
+echo "Value of c"
+read c 
+echo "Hello value of a is $a and value of b is $b and value of c is &c "
 
