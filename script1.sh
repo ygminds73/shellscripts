f846e0d81ffd923f5010b22a2b54be6360d1599e
shitalkalyankar <shitalkalyankar1995@gmail.com>
2024-08-20 15:10:15 +0530

    This is my first practical

--- script1.sh
+++ script1.sh
@@ -1,5 +1,6 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Git/Github"
-
+echo "hello friends"
+echo "Good Afternoon"
 
