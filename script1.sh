f7cff3d22537f4e57314313259c1f74305bc3945
YoungMinds <ygminds73@gmail.com>
2024-08-20 09:26:12 +0530

    This is my first commit

--- script1.sh
+++ script1.sh
@@ -1,8 +1,5 @@
 echo "We are trying this practical from linux EC2"
-echo "We are from batch-24 and learning git/gihub"
-echo "Happy weekend"
-echo "We are learning git"
-echo "Husband: I don't want to travel"
-echo "Wife: I want to travel"
+echo "We are from batch-25 and learning git/gihub"
+echo "We are learning Git/Github"
 
 
