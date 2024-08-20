9103227b9f4be3be95b46a3b7781bfbbc5b4360a
JAY <jayrajepandule2210@gmail.com>
2024-08-20 22:41:34 +0530

    changed are commited

--- script1.sh
+++ script1.sh
@@ -2,4 +2,5 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Git/Github"
 
-
+echo "my name is jay"
+echo "this is jay"
