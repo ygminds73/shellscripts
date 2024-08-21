099b162f892c9e4b933a82091a82b9aa58fc59de
root <root@ip-172-31-3-28.ap-south-1.compute.internal>
2024-08-21 09:33:35 +0000

    Changes in Script-1

--- script1.sh
+++ script1.sh
@@ -2,5 +2,5 @@ echo "Good Morning"
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Git/Github/JAVA"
-
+echo "We are learning Git on Linux"
 
