1b683d4c99201011868351956dcd040d805f1375
JAY <jayrajepandule2210@gmail.com>
2024-08-21 11:50:32 +0000

    my inbox

--- script1.sh
+++ script1.sh
@@ -1,6 +1,5 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Git/Github"
-
-echo "my name is jay"
-echo "this is jay"
+echo "my name is sham"
+echo "this is ram"
