f4a3a2300c5ae40ed8c7167984b4b50cc8d0ec83
harinisiriki <harinisiriki123@gmail.com>
2024-08-21 10:28:33 +0530

    Resolved issue related with REQ56789

--- script1.sh
+++ script1.sh
@@ -2,5 +2,4 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Git/Github"
 echo "Currently learning DevOps tools"
-
-
+echo "Learnt branching in git"
