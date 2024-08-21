2b09064d0e6549dbcdc1f313fc562708c0519bc8
shankar <shankarpawse970@gmail.com>
2024-08-21 22:41:09 +0530

    change my message

--- script1.sh
+++ script1.sh
@@ -2,4 +2,4 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Git/Github"
 echo "di"
-
+echo "hello i am new learnig github"
