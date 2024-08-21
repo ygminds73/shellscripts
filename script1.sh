fcbd8b868355cb346667d3cd53226683d20e90d7
JAY <jayrajepandule2210@gmail.com>
2024-08-21 22:15:59 +0530

    changed some

--- script1.sh
+++ script1.sh
@@ -1,5 +1,6 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Git/Github"
-echo "my name is sham"
-echo "this is ram"
+echo "my name is Bholenath"
+echo "this is mahadev"
+echo "we are learning branching"
