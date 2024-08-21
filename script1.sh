0dcb37108056e3cb583808dd30b24b71c41a26db
root <root@ip-172-31-33-192.ec2.internal>
2024-08-21 15:36:50 +0000

    added good morning

--- script1.sh
+++ script1.sh
@@ -1,6 +1,4 @@
 echo "We are trying this practical from linux EC2"
-echo "We are from batch-25 and learning git/gihub"
-echo "We are learning Git/Github"
 echo "We are learning devops"
-
+echo "We are trying to add some files"
 
