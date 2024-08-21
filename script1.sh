80cf5fc5b6169b55e32c8bd6e03303be3158f67c
Yukta <yuktakadu113@gmail.com>
2024-08-21 21:16:53 +0530

    good girl

--- script1.sh
+++ script1.sh
@@ -1,5 +1,4 @@
 echo "We are trying this practical from linux EC2"
 echo "We are learning devops"
-echo "We are trying to add some files"
-echo "we are learning to create branch"
+echo "good girl"
 
