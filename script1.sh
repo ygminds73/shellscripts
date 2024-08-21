4d79f78df2308d52eb46057b91e60358815dee0f
root <root@ip-172-31-7-225.ap-south-1.compute.internal>
2024-08-21 17:53:12 +0000

    changes in script 1

--- script1.sh
+++ script1.sh
@@ -2,4 +2,5 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Git/Github"
 echo "We are learning devops"
+echo "We are learning today new topic of devops as braching"
 
