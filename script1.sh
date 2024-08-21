0b97fb088ef32e6221bc102178f514af6f170f62
root <root@ip-172-31-7-222.ec2.internal>
2024-08-21 10:09:28 +0000

    change the commit

--- script1.sh
+++ script1.sh
@@ -1,6 +1,9 @@
-echo "We are trying this practical from linux EC2"
+necho "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Git/Github"
 
 echo "my name is jay"
 echo "this is jay"
+echo "hoo god"
+echo  "my name is jaydeep"
+
