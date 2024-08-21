2e13b566b5c6b315895ec243f13ccc5627961181
Sumit <sumittayade3@gmail.com>
2024-08-21 10:59:50 +0000

    My INBOX

--- script1.sh
+++ script1.sh
@@ -3,4 +3,4 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Git/Github/JAVA"
 echo "We are learning Git on Linux"
-
+echo "We are "INDIANS:"
