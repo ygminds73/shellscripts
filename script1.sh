6836eaff37cc26d17598d8c3a80cec96c807b3fd
Rahul.salunke <rahulsalunke75@gmail.com>
2024-08-22 00:50:09 +0530

    Braching

--- script1.sh
+++ script1.sh
@@ -3,4 +3,5 @@ echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Git/Github"
 echo "We are learning devops"
 echo "We are learning today new topic of devops as braching"
+echo "We are working on branch request INC701234"
 
