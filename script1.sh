93a7fe6690971ac6fbdbca04c5df6803d0451a34
JAY <jayrajepandule2210@gmail.com>
2024-08-21 11:05:43 +0000

    change the

--- script1.sh
+++
@@ -1,9 +0,0 @@
-necho "We are trying this practical from linux EC2"
-echo "We are from batch-25 and learning git/gihub"
-echo "We are learning Git/Github"
-
-echo "my name is jay"
-echo "this is jay"
-echo "hoo god"
-echo  "my name is jaydeep"
-
