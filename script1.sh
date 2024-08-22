25c721fe546d7bf22762512958b9163244b3fa05
Ashish <ashishgorde199@gmail.com>
2024-08-22 19:08:59 +0530

    this is for prac

--- script1.sh
+++ script1.sh
@@ -2,5 +2,6 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Git/Github"
 echo "This is in my Remote Repo"
+echo "This is Ashish Gordea Repo"
 
 
