60303ac91967a9200ffe7e8c108c5e64c927aef4
rohit <Rohitshimpi07@gmail.com>
2024-08-22 20:07:50 +0530

    this is my first change

--- script1.sh
+++ script1.sh
@@ -1,6 +1,4 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Branhcing"
-echo "I am learning Devops"
-echo "I am learning Git"
-
+echo "i am playing football"
