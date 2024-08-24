9d2548b01e1ca93bac1935ea5329a02581e97dbb
dhanupatil11 <dhanupatil180@gmail.com>
2024-08-24 12:02:54 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -6,6 +6,9 @@ echo "We are from batch-25 and learning git/gihub3"
 echo "We are from batch-25 and learning git/gihub4"
 echo "We are from batch-25 and learning git/gihub5"
 echo "We are from batch-25 and learning git/gihub6"
+snfckf,dfk
+ncs
+m, 
 
 
 
