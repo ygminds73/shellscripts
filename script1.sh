e6526e0655acf8c1301c85c0f684d2409cf5eb92
dhanupatil11 <dhanupatil180@gmail.com>
2024-08-24 11:03:22 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,3 +1,7 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
+echo "We are from batch-25 and learning git/gihub1"
+echo "We are from batch-25 and learning git/gihub2"
+
+
 
