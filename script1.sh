f4b8a46078aea1eeb03345800074ab1c309e65e7
dhanupatil11 <dhanupatil180@gmail.com>
2024-08-24 11:39:41 +0530

    changed

--- script1.sh
+++ script1.sh
@@ -4,6 +4,8 @@ echo "We are from batch-25 and learning git/gihub1"
 echo "We are from batch-25 and learning git/gihub2"
 echo "We are from batch-25 and learning git/gihub3"
 echo "We are from batch-25 and learning git/gihub4"
+echo "We are from batch-25 and learning git/gihub5"
+echo "We are from batch-25 and learning git/gihub6"
 
 
 
