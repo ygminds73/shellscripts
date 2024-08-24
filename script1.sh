c7efa69d9b2e1935de9121561db5155d8736c667
Akash-M-Ubale <akashubale70@gmail.com>
2024-08-24 15:21:11 +0530

    THIS IS MY COMMIT TO TEH REPO

--- script1.sh
+++ script1.sh
@@ -1,4 +1,5 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "new practical is been processed again"
-
+echo "abcdeeegjlgjdljdojdljglgjgolj;o"
+echo "n1232656454656956"
