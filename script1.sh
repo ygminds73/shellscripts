cec9effea955c9c519e0394c2ffb9e1ed45873c3
Pratik <pratikzende882002@gmail.com>
2024-08-25 12:39:11 +0000

    commit

--- script1.sh
+++ script1.sh
@@ -1,4 +1,5 @@
 echo "Good Morning"
+echo "Welcome"
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "we are trying practical from Linux"
