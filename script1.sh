da4afb0402a8bc1f057e00c9314a07d6962b63be
root <root@ip-172-31-91-123.ec2.internal>
2024-08-26 05:28:20 +0000

    this is my last commit

--- script1.sh
+++ script1.sh
@@ -1,5 +1,3 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Branching"
-echo "i am playing football"
-echo "i am dancing"
