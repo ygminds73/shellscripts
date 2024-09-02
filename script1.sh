80e4cad7028a7370f5b3f07d512c8340944cdc49
Aishu23032000 <aishwaryadeshpande2303@gmail.com>
2024-09-02 14:06:49 +0530

    commit is done

--- script1.sh
+++ script1.sh
@@ -1,6 +1,6 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
-echo "Hello Everyone"
-echo "We are learning branching"
+echo "I am learning branching today "
+echo "Bye"
 
 
