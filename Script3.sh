af472cf45380d609bd539ef66507c7264de91594
Sumit <sumitpt1995@rediffmail.com>
2024-09-19 10:29:30 -0700

    change in script3

--- Script3.sh
+++ Script3.sh
@@ -2,7 +2,7 @@
 #this is to check if the shell scripts picks up env variables
 echo "This is error script"
 echo "This is my system path $PATH"
-
+echo "This is error script2"
 #Set a JDK_HOME env variable
 export JDK_HOME=/bin/jdk
 echo "my new JDK home is=$JDK_HOME"
