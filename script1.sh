042f3d43c75d6e489aef290383de71de25173bd7
Prafulljankar <prafulljankar9665@gmail.com>
2024-10-04 20:39:10 +0530

    The Change are commited

--- script1.sh
+++ script1.sh
@@ -1,5 +1,5 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/github"
 echo "good morning"
-echo "Nice Job"
 echo "We are Selecting for this role"
+echo "Nice Job"
