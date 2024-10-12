3426c7c4d33732ba01ce0f1f1970544786b0cdc2
Bhushan <bhushanshinde607@gmail.com>
2024-10-12 14:45:01 +0530

    This is New commited file

--- script1.sh
+++ script1.sh
@@ -1,5 +1,6 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "we have attend github class"
+echo "we are learning git/github''
 echo "Thank You"
 
