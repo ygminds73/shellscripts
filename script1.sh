c35a542abdc19e6abc630cbb38bcdc9229b3ac25
root <root@ip-172-31-45-29.ec2.internal>
2024-10-12 10:21:26 +0000

    This New One

--- script1.sh
+++ script1.sh
@@ -1,6 +1,7 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "we have attend github class"
-echo "we are learning git/git hub 2"
-echo "Thank You Team"
+echo "Hello Guys"
+echo "Good Evening, we are learning new topic"
+echo "Thank You"
 
