8785d1cc7cb1c23b39c2706f313a89387a3a60f7
Your Name <you@example.com>
2024-11-14 22:45:07 +0530

    Problem is resolved

--- Scrpit10.sh
+++ Scrpit10.sh
@@ -8,7 +8,6 @@ echo "a is greater than b"
 else
 echo "a is smaller than b"
 fi
-echo " Hi Tom,What are you doing ? "
-mkdir onkar
-cd onkar
-vim text.
+
+echo "Kolhapur is very big city"
+echo "We are maharashtrian"
