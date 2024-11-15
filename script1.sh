1654a8931f99e48934d592295c3f26b8cb029c2f
YoungMinds <ygminds73@gmail.com>
2024-11-15 08:16:50 +0530

    I am working on branching

--- script1.sh
+++ script1.sh
@@ -1,4 +1,5 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are from batch-26 and learning DevOps"
-echo "Hello, Goodmorning!"
+echo "Hello, Good morning!"
+echo "Hello, we are learning Git and GITHub"
